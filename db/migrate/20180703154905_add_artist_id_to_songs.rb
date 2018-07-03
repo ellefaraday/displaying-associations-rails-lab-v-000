1b0a77a052f88135a1b6e8d9123b99740b554fa6
Elle Faraday <faraday.elle@gmail.com>
2018-07-03 15:49:41 +0000

    automatically backed up by learn

--- db/migrate/20180703154905_add_artist_id_to_songs.rb
+++ db/migrate/20180703154905_add_artist_id_to_songs.rb
@@ -1,6 +1,5 @@
 class AddArtistIdToSongs < ActiveRecord::Migration
   def change
-    add_column :songs, :artist_id, :string
-    add_column :songs, :string, :string
+    add_column :songs, :artist_id, :integer
   end
 end
